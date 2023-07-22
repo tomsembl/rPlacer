0db2e1396dc7118601b91353bfc24c5b4f981817
tomsembl <94670548+tomsembl@users.noreply.github.com>
2023-07-22 18:33:05 +0800

    Create rPlacer.js

---
+++ rPlacer.js
@@ -0,0 +1,26 @@
+function place() {
+    try {
+        containerParent = document.querySelector("body > garlic-bread-app > faceplate-alert-reporter > garlic-bread-embed")
+        if (containerParent == null) {console.error('To fix this error, go to the Elements tab. Ctrl+F, type "main-text" and hit enter (this step makes the shadow dom accessible to JS)'); return}
+        container = containerParent.shadowRoot.querySelector("div > garlic-bread-share-container")
+
+        placeButton = container.querySelector("div.bottom-controls > garlic-bread-status-pill").shadowRoot.querySelector("button")
+        if (placeButton.querySelector("div > div.main-text").innerHTML != ' Place! ') {console.log("waiting"); return}
+        placeButton.click()
+
+        colourPickerMenu = container.querySelector("garlic-bread-color-picker").shadowRoot
+        orangeButton = colourPickerMenu.querySelector("div > div > div.palette > div:nth-child(2) > button")
+        //to change the colour, change the number in "div:nth-child(2)" eg. 1=red, 2=orange, 3=yellow
+        orangeButton.click()
+
+        confirmButton = colourPickerMenu.querySelector("div > div > div.actions > button.confirm.disable-default-select")
+        confirmButton.click()
+    } catch (err) {}
+}
+// start the script
+autoPlacer = setInterval(place, 1000)
+
+/* 
+to stop the script: 
+clearInterval(autoPlacer) 
+*/
